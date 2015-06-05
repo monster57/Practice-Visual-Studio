79b5866bdc7fa1998b03a937093a398cc7b5afa2
monster57 <surajitbarman57@yahoo.in>
2015-06-05 10:32:44 +0530

    reading and writing practice is done in console.

--- ReadingAndWritingInConsole/ReadingAndWritingInConsole/Program.cs
+++ ReadingAndWritingInConsole/ReadingAndWritingInConsole/Program.cs
@@ -10,6 +10,13 @@ namespace ReadingAndWritingInConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please Enter Your Name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Please Enter The Last Name");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("hello " + userName);
+            Console.WriteLine("hello {0} {1}" , userName , LastName);
+            Console.ReadLine();
         }
     }
 }
